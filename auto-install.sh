b9bb96050988ac725af41ddc1e23ee7b9190e653
codjust <1040385974@qq.com>
2018-06-23 01:21:54 +0800

    modify scripts
    use by myself

--- auto-install.sh
+++ auto-install.sh
@@ -14,7 +14,7 @@ die() {
 [ -e "~/.vim" ] && die "~/.vim already exists."
 [ -e "~/.vimrc" ] && die "~/.vimrc already exists."
 
-git clone git://github.com/vgod/vimrc.git "$VIMHOME"
+git clone git@github.com:huchangwei/vimrc-1.git "$VIMHOME" 
 cd "$VIMHOME"
 git submodule update --init
 
